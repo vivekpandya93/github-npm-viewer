4bff90ac1b74545de9b7e82010ce655a4bf84e7e
Vivek Pandya <vivekpandya@Viveks-MacBook-Pro.local>
2016-05-20 19:31:46 -0700

    cleaned up gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -51,36 +51,6 @@ gulp.task('buildJS', ['lintJS'], function () {
         .pipe(gulp.dest('./public'));
 });
 
-gulp.task('testServerJS', function () {
-    require('babel/register');
-	return gulp.src('./tests/server/**/*.js', {
-		read: false
-	}).pipe(mocha({ reporter: 'spec' }));
-});
-
-gulp.task('testServerJSWithCoverage', function (done) {
-    gulp.src('./server/**/*.js')
-        .pipe(istanbul({
-            includeUntested: true
-        }))
-        .pipe(istanbul.hookRequire())
-        .on('finish', function () {
-            gulp.src('./tests/server/**/*.js', {read: false})
-                .pipe(mocha({reporter: 'spec'}))
-                .pipe(istanbul.writeReports({
-                    dir: './coverage/server/',
-                    reporters: ['html', 'text']
-                }))
-                .on('end', done);
-        });
-});
-
-gulp.task('testBrowserJS', function (done) {
-    karma.start({
-        configFile: __dirname + '/tests/browser/karma.conf.js',
-        singleRun: true
-    }, done);
-});
 
 gulp.task('buildCSS', function () {
 
@@ -148,11 +118,6 @@ gulp.task('default', function () {
     // Reload when a template (.html) file changes.
     gulp.watch(['browser/**/*.html', 'server/app/views/*.html'], ['reload']);
 
-    // Run server tests when a server file or server test file changes.
-    gulp.watch(['tests/server/**/*.js'], ['testServerJS']);
-
-    // Run browser testing when a browser test file changes.
-    gulp.watch('tests/browser/**/*', ['testBrowserJS']);
 
     livereload.listen();
 
